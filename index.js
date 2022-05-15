66ddc15883ad0047a40a671ea59e898f16fe3335
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-15 13:13:59 -0500

    Agregando ruta que muestre un json de prueba

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ app.listen(3000, () => {
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
+//RUTAS
+app.get('/pages', (request, response) => {
+    response.json({
+        pages: ['prueba1','prueba2']
+    });
+});
